ef928570289047a00624f7545f51b9ad75459c67
Emmaculate oduor <oduoremmaculate75@gmail.com>
2025-05-04 22:30:24 +0300

    Week 7 assignment submission

---
+++ answers.sql
@@ -0,0 +1,60 @@
+-- QUESTION 1
+
+/*In the table above, the **Products column** contains multiple values, which violates **1NF**.
+**Write an SQL query** to transform this table into **1NF**, ensuring that each row represents a single product for an order*/
+
+CREATE DATABASE normalization;
+
+CREATE TABLE ProductDetail (
+    OrderID INT,
+    CustomerName VARCHAR(100) NOT NULL,
+    Product VARCHAR(100) NOT NULL
+);
+
+INSERT INTO ProductDetail (OrderID, CustomerName, Product)
+VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
+
+
+
+-- QUESTION 2
+
+
+/*Write an SQL query to transform this table into **2NF** by removing partial dependencies. Ensure that each non-key column fully depends on the entire primary key.*/
+
+
+-- Created Orders table ( to remove partial dependency)
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
+);
+
+
+INSERT INTO Orders (OrderID, CustomerName)
+VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
+
+-- Created OrderProducts table (to add remaining data)
+CREATE TABLE OrderProducts (
+    OrderID INT,
+    Product VARCHAR(100),
+    Quantity INT,
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+
+INSERT INTO OrderProducts (OrderID, Product, Quantity)
+VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
